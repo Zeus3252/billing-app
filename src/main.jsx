e7bbb03c801d3ef2d1f4acfb9a5bd0adec2616fa
Jayson Brown <jayb3252@gmail.com>
2024-05-17 21:57:27 -0400

    gh-pages deploy

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename="/billing-app/">
+    <Router>
       <AppProvider>
         <App />
       </AppProvider>
